17cf050bd8e226bd6c466b830b2d38fd67ec15f8
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-03-12 15:57:22 -0300

    Updating scripts

--- scripts/hydra-operations/purge.sh
+++ scripts/hydra-operations/purge.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
 
-function destroy_image() {
-
-}
-
 function destroy_images() {
   if [[ " ${DOCKER_CONTAINERS[*]} " =~ "data-l1" ]]; then
-    destroy_container data-l1 "DATA-L1"
-    rm -f infra/docker/shared/jars/data-l1.jar
+    docker rmi data-l1-initial-validator
+    docker rmi data-l1-validator-node-2
+    docker rmi data-l1-validator-node-3
   fi
 
   if [[ " ${DOCKER_CONTAINERS[*]} " =~ "currency-l1" ]]; then
-    destroy_container currency-l1 "CURRENCY-L1"
-    rm -f infra/docker/shared/jars/currency-l1.jar
+    docker rmi currency-l1-initial-validator
+    docker rmi currency-l1-validator-node-2
+    docker rmi currency-l1-validator-node-3
   fi
 
   if [[ " ${DOCKER_CONTAINERS[*]} " =~ "metagraph-l0" ]]; then
-    destroy_container metagraph-l0/genesis "METAGRAPH-L0-GENESIS"
-    destroy_container metagraph-l0 "METAGRAPH-L0-VALIDATORS"
-    rm -f infra/docker/shared/jars/metagraph-l0.jar
+    docker rmi metagraph-l0-initial-validator
+    docker rmi metagraph-l0-validator-node-2
+    docker rmi metagraph-l0-validator-node-3
   fi
 
   if [[ " ${DOCKER_CONTAINERS[*]} " =~ "dag-l1" ]]; then
-    destroy_container dag-l1 "DAG-L1"
-    rm -f infra/docker/shared/jars/dag-l1.jar
+    docker rmi dag-l1-initial-validator
+    docker rmi dag-l1-validator-node-2
+    docker rmi dag-l1-validator-node-3
   fi
 
   if [[ " ${DOCKER_CONTAINERS[*]} " =~ "global-l0" ]]; then
-    destroy_container global-l0 "GLOBAL-L0"
-    rm -f infra/docker/shared/jars/global-l0.jar
+    docker rmi global-l0
   fi
 
   if [[ " ${DOCKER_CONTAINERS[*]} " =~ "monitoring" ]]; then
-    destroy_container monitoring "MONITORING"
+    docker rmi grafana/grafana-oss
+    docker rmi prom/prometheus
   fi
 
-  destroy_container metagraph-base-image "BASE-IMAGE"
+  docker rmi $(docker images -q "metagraph-base-image-*")
 }
 
 function purge_containers() {
   echo_white "Starting purging containers ..."
   destroy_containers
+  destroy_images
 }
