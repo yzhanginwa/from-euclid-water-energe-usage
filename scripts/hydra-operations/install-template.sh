5586688c5ed97e56a42be20aea173dbcf9b51c0f
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-03-13 17:54:47 -0300

    Adding --list to install-template

--- scripts/hydra-operations/install-template.sh
+++ scripts/hydra-operations/install-template.sh
@@ -2,6 +2,29 @@ function install_template() {
   repo_name_with_git=$(basename "$argc_repo")
   repo_name="${repo_name_with_git%.git}"
 
+  if [ "${argc_list}" ]; then
+    echo_yellow "Fetching templates..."
+    
+    cd $INFRA_PATH
+    rm -r -f $INFRA_PATH/$repo_name
+    git clone --quiet $argc_repo >/dev/null
+    
+    echo_yellow ""
+    echo_yellow "Available templates"
+    echo_green ""
+    ls -1 $INFRA_PATH/$repo_name/$argc_path
+    
+    rm -rf $INFRA_PATH/$repo_name
+    
+    echo_green ""
+    exit 0
+  fi
+
+  if [ -z "${argc_name}" ]; then
+    echo_red "You should provide the repository name"
+    exit 1
+  fi
+
   echo_green "##########################################"
   echo_url "Project name:" $argc_name
   echo_url "Repository URL:" $argc_repo
@@ -25,27 +48,25 @@ function install_template() {
   cd $repo_name/$argc_path
 
   echo_white "Cleaning old directories: template or $argc_name from projects"
-  rm -r -f $SOURCE_PATH/project/template
   rm -r -f $SOURCE_PATH/project/$argc_name
 
   echo_white "Moving template to the projects directory"
-  mv -n -f $argc_name/template $SOURCE_PATH/project
-  mv $SOURCE_PATH/project/template $SOURCE_PATH/project/$argc_name
+  mv -n -f $argc_name $SOURCE_PATH/project
 
   echo_white "Updating euclid.json project_name"
   contents="$(jq --arg PROJECT_NAME "$argc_name" '.project_name = $PROJECT_NAME' $ROOT_PATH/euclid.json)" &&
     echo -E "${contents}" >$ROOT_PATH/euclid.json
 
   echo_white "Updating euclid.json tessellation_version"
-  project_tessellation_version=$(grep "val tessellation =" $SOURCE_PATH/project/$argc_name/project/Dependencies.scala | awk -F '"' '{print $2}') 
+  project_tessellation_version=$(grep "val tessellation =" $SOURCE_PATH/project/$argc_name/project/Dependencies.scala | awk -F '"' '{print $2}')
   contents="$(jq --arg PROJECT_TESSELLATION_VERSION "$project_tessellation_version" '.tessellation_version = $PROJECT_TESSELLATION_VERSION' $ROOT_PATH/euclid.json)" &&
     echo -E "${contents}" >$ROOT_PATH/euclid.json
 
   rm -r -f $INFRA_PATH/$repo_name
 
-  cd $ROOT_PATH
-  if [ -d ".git" ]; then
-    chmod -R +w .git
-    rm -r .git
-  fi
+  # cd $ROOT_PATH
+  # if [ -d ".git" ]; then
+  #   chmod -R +w .git
+  #   rm -r .git
+  # fi
 }
